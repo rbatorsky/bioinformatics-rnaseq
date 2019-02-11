d696b191de97d59e5184e161f91863434b75a8fe
rbatorsky <rebecca.batorsky@tufts.edu>
2019-02-11 15:26:37 -0500

    added scripts directory

---
+++ scripts/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+module unload openmpi
+module load fastqc/0.11.8
+module load subread/1.6.3
+module load anaconda
+module load samtools/1.2
+module load STAR/2.7.0a
+source activate /cluster/kappa/90-days-archive/bio/tools/conda_envs/rnaseq_course
