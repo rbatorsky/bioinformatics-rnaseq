69fa1c338e34a4440d8fbe0ab3ef0b4514d023c1
rbatorsky <rebecca.batorsky@tufts.edu>
2019-02-11 15:52:57 -0500

    added scripts directory

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,3 +6,5 @@ module load anaconda
 module load samtools/1.2
 module load STAR/2.7.0a
 source activate /cluster/kappa/90-days-archive/bio/tools/conda_envs/rnaseq_course
+mkdir course_data
+cp /cluster/kappa/90-days-archive/bio/tools/tutorials/bioinformatics-rnaseq-spring-20109/* course_data/
