007b32480ed18c976e54e6410457cdf2688277f3
rbatorsky <rebecca.batorsky@tufts.edu>
2019-02-13 11:53:11 -0500

    add two download scripts

---
+++ scripts/download_samples_forloop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for acc in ERR458493 ERR458494 ERR458495 ERR458496 ERR458497 ERR458498;
+do
+	echo $acc
+	egrep ${acc} samples_at_ENA.txt | cut -f11 | xargs wget
+done
+
---
+++ scripts/download_samples_whileloop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while read $acc; 
+do 
+	echo $acc
+	egrep $acc samples_at_ENA.txt| cut -f11 | xargs wget
+done< <(cat ERP004763_sample_mapping.tsv | awk '$4 == 1' | awk '$3 == WT'  | awk '{print $1}')
